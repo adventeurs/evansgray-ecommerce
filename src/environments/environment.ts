d72cbef6e19c04f222aa1699a2d8facfc1788023
Justin Miller <adventeurs@gmail.com>
2020-06-17 23:23:56 -0500

    heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   stripeKey: "pk_test_e2tsWERbw2LAQZDpvfaRsNIM00ezTkbWOQ",
   firebase: {
     apiKey: "AIzaSyAhVkuXB480h5R55Uo2o1thhZu1QKWSzD0",
@@ -25,5 +25,3 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
-
-
