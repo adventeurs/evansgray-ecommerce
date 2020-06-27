008a993efa470e3800e9b309ef284ad679a4e27d
Justin Miller <adventeurs@gmail.com>
2020-06-26 22:15:58 -0500

    fix: coupon route in node

--- server/routes/payments/coupon.js
+++ server/routes/payments/coupon.js
@@ -3,7 +3,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET);
 module.exports = async (req, res) => {
   const { code } = req.body;
   try {
-    let discount = awaitstripe.coupons.retrieve(code);
+    let discount = await stripe.coupons.retrieve(code);
     res.send(discount);
   } catch (err) {
     res.send(err);
