1f4cc02e6130694d6b29f6e5bb80a2ba8f7707f4
Justin Miller <adventeurs@gmail.com>
2020-08-12 23:49:42 -0500

    prepare checkout service for reorganization

--- src/app/payments-module/checkout/shipping-info/shipping-info.component.ts
+++ src/app/payments-module/checkout/shipping-info/shipping-info.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-shipping-info',
-  templateUrl: './shipping-info.component.html',
-  styleUrls: ['./shipping-info.component.scss']
+  selector: "app-shipping-info",
+  templateUrl: "./shipping-info.component.html",
+  styleUrls: ["./shipping-info.component.scss"]
 })
-export class ShippingInfoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class ShippingInfoComponent {}
